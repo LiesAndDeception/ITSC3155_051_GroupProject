e390067f34ab5df8437e373009c2c93b9483dab3
Johnson Luu <jluu2@charlotte.edu>
2025-04-13 13:24:14 -0400

    feat(schemas): define Pydantic schemas for Review creation and output

--- api/schemas/reviews.py
+++ api/schemas/reviews.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
 
+
 class ReviewBase(BaseModel):
     customer_id: int
     review_text: str
     score: int
 
+
 class ReviewCreate(ReviewBase):
     pass
+
 
 class ReviewOut(ReviewBase):
     id: int
 
     class Config:
         orm_mode = True
+

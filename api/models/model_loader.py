943f107efccbd055966bc9fa0e25de5bc89244fd
QueenSophiaLo <75154780+QueenSophiaLo@users.noreply.github.com>
2025-04-14 21:12:46 -0400

    refactor: remove sandwiches module from database initialization

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources, payment_information, resource_management
+from . import orders, order_details, recipes, resources, payment_information, resource_management
 
 from ..dependencies.database import engine
 
@@ -7,7 +7,6 @@ def index():
     orders.Base.metadata.create_all(engine)
     order_details.Base.metadata.create_all(engine)
     recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
     payment_information.Base.metadata.create_all(engine)
     resource_management.Base.metadata.create_all(engine)

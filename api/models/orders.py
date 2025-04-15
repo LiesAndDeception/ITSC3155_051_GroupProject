46ba4397144ebb57d0e16173575d367012fefba5
QueenSophiaLo <75154780+QueenSophiaLo@users.noreply.github.com>
2025-04-14 21:05:58 -0400

    feat(order): add tracking number, status, and total price to model

--- api/models/orders.py
+++ api/models/orders.py
@@ -9,7 +9,11 @@ class Order(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     customer_name = Column(String(100))
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    order_date = Column(DATETIME, nullable=False, default=datetime.utcnow)
     description = Column(String(300))
+
+    tracking_number = Column(String(50), nullable=True)
+    order_status = Column(String(50), nullable=False, default="Processing")
+    total_price = Column(DECIMAL(10, 2), nullable=False, default=0.00)
 
     order_details = relationship("OrderDetail", back_populates="order")

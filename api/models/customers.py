5ac4cf96afc14f1d1502b187b6f8d8f2202d554f
Jluu26 <jluu2@charlotte.edu>
2025-05-07 12:40:23 -0400

    Establish relationship between Customer and Order models

--- api/models/customers.py
+++ api/models/customers.py
@@ -12,3 +12,5 @@ class Customer(Base):
     email = Column(String(500), unique=True, nullable=False)
     phone = Column(String(25), nullable=False)
     address = Column(String(500), nullable=True)
+
+    orders = relationship("Order", back_populates="customer")
